8b2ac6048f785d3ff3b26f8d6a656bbfd13647c1
someone7140 <ti7140@mail.goo.ne.jp>
2022-05-13 01:01:01 +0900

    CORS対応

--- place-api/src/main.rs
+++ place-api/src/main.rs
@@ -1,3 +1,5 @@
+use actix_cors::Cors;
+use actix_web::http;
 use actix_web::App;
 use actix_web::HttpServer;
 
@@ -17,8 +19,16 @@ mod model {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().service(controller::search_controller::get_search_condition))
-        .bind("0.0.0.0:8080")?
-        .run()
-        .await
+    HttpServer::new(|| {
+        let cors = Cors::default()
+            .allowed_origin("http://localhost:19006")
+            .allowed_methods(vec!["GET", "POST", "PUT", "OPTIONS", "DELETE"])
+            .allowed_header(http::header::CONTENT_TYPE);
+        App::new()
+            .wrap(cors)
+            .service(controller::search_controller::get_search_condition)
+    })
+    .bind("0.0.0.0:8080")?
+    .run()
+    .await
 }
